1eca5c414e4c193224ec7af99a38d7b9017a8231
ari0129 <82903510+ari0129@users.noreply.github.com>
2021-05-10 13:42:49 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
 /**
  * At anytime, you can right-click on a block and select "help" for more info on the specific block and how it can be used.
  */
-/**
- * To comment in your JS, use the double /
- */
+// To comment in your JS, use the double /
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     game.over(true)
 })
